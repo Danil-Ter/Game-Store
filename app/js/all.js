39bee35593b53dd928e7bdfa1d65d74c7bf7575a
Danya-molodoy <114100824+Danya-molodoy@users.noreply.github.com>
2022-10-10 18:34:56 +0300

    parallax and sticky header

--- app/js/all.js
+++ app/js/all.js
@@ -9,11 +9,29 @@ $('.customize__slider').slick({
     slidesToScroll: 1
   });
 
-
-
   const cards = document.querySelectorAll('.js-card');
 
   VanillaTilt.init(cards, {
     max: 10,
     glare:false,
 });
+
+let lastScroll = 0;
+const defaultOffset = 10;
+const header = document.querySelector('.header');
+
+const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
+const containHide = () => header.classList.contains('header__scrol');
+
+window.addEventListener('scroll', () => {
+    if(scrollPosition() > lastScroll && !containHide() && scrollPosition() > defaultOffset) {
+        //scroll down
+        header.classList.add('header__scrol');
+    }
+    else if(scrollPosition() < lastScroll && containHide()){
+        //scroll up
+        header.classList.remove('header__scrol');
+    }
+
+    lastScroll = scrollPosition();
+})
