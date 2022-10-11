92004bb1862f22611da62814e7086ff68e413a2e
Danya-molodoy <114100824+Danya-molodoy@users.noreply.github.com>
2022-10-11 21:21:36 +0300

    added animations when scrolling

--- app/js/script.js
+++ app/js/script.js
@@ -10,23 +10,3 @@ $('.customize__slider').slick({
     max: 10,
     glare:false,
 });
-
-let lastScroll = 0;
-const defaultOffset = 10;
-const header = document.querySelector('.header');
-
-const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
-const containHide = () => header.classList.contains('header__scrol');
-
-window.addEventListener('scroll', () => {
-    if(scrollPosition() > lastScroll && !containHide() && scrollPosition() > defaultOffset) {
-        //scroll down
-        header.classList.add('header__scrol');
-    }
-    else if(scrollPosition() < lastScroll && containHide()){
-        //scroll up
-        header.classList.remove('header__scrol');
-    }
-
-    lastScroll = scrollPosition();
-})
